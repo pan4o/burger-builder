5a6bad38f47dcc7bf2f18f24487d9fa5ed603a06
pan4o <panchenko923@gmail.com>
2019-03-04 23:05:48 +0200

    add hoc

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,7 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHander from '../../hoc/withErrorHander/withErrorHander';
 import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
@@ -167,4 +168,4 @@ class BurgerBuilder extends Component {
   }
 }
 
-export default BurgerBuilder;
+export default withErrorHander(BurgerBuilder, axios);
